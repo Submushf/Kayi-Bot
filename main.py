6c3f7728a326372c30a15f792ae97dcf1b9c1319
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2021-02-26 23:22:39 +0600

    new

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ async def ertugrul(ctx):
     random_image =  random.choice(images) 
     await ctx.send(file = discord.File(random_image))
 images = [
-    "Pics\Ertugrul\ertugrul 1.jpg","Pics\Ertugrul\ertugrul 2.jpg","Pics\Ertugrul\ertugrul 3.jpg",
-    "Pics\Ertugrul\ertugrul 4.jpg","Pics\Ertugrul\ertugrul 5.jpg","Pics\Ertugrul\ertugrul 6.jpg"
+    "Pics\\Ertugrul\\ertugrul 1.jpg","Pics\\Ertugrul\\ertugrul 2.jpg","Pics\\Ertugrul\\ertugrul 3.jpg",
+    "Pics\\Ertugrul\\ertugrul 4.jpg","Pics\\Ertugrul\\ertugrul 5.jpg","Pics\\Ertugrul\\ertugrul 6.jpg"
     ]
 
 @client.command()
@@ -36,8 +36,8 @@ async def osman(ctx):
 
     await ctx.send(file = discord.File(random_image))
 osma = [
-    "Pics\Osman\osman 1.jpg","Pics\Osman\osman 2.jpg","Pics\Osman\osman 3.jpg",
-    "Pics\Osman\osman 4.jpg","Pics\Osman\osman 5.jpg","Pics\Osman\osman 6.jpg" 
+    "Pics\\Osman\\osman 1.jpg","Pics\\Osman\\osman 2.jpg","Pics\\Osman\\osman 3.jpg",
+    "Pics\\Osman\\osman 4.jpg","Pics\\Osman\\osman 5.jpg","Pics\\Osman\\osman 6.jpg" 
 ]
 
 
@@ -48,8 +48,8 @@ async def ibn_arabi(ctx):
 
     await ctx.send(file = discord.File(random_image))
 ibn = [
-    "Pics\IbnArabi\Ibn 1.jpg","Pics\IbnArabi\Ibn 2.jpg","Pics\IbnArabi\Ibn 3.jpg",
-    "Pics\IbnArabi\Ibn 4.jpg","Pics\IbnArabi\Ibn 5.jpg","Pics\IbnArabi\Ibn 6.jpg"
+    "Pics\\IbnArabi\\Ibn 1.jpg","Pics\\IbnArabi\\Ibn 2.jpg","Pics\\IbnArabi\\Ibn 3.jpg",
+    "Pics\\IbnArabi\\Ibn 4.jpg","Pics\\IbnArabi\\Ibn 5.jpg","Pics\\IbnArabi\\Ibn 6.jpg"
 ]
 
 @client.command()
@@ -59,8 +59,8 @@ async def bala(ctx):
 
     await ctx.send(file = discord.File(random_image))
 balah = [
-    "Pics\Bala\Bala 1.jpg", "Pics\Bala\Bala 2.jpg","Pics\Bala\Bala 3.jpg",
-    "Pics\Bala\Bala 4.jpg","Pics\Bala\Bala 5.jpg"
+    "Pics\\Bala\\Bala 1.jpg", "Pics\\Bala\\Bala 2.jpg","Pics\\Bala\\Bala 3.jpg",
+    "Pics\\Bala\\Bala 4.jpg","Pics\\Bala\\Bala 5.jpg"
 ]
 
 @client.command()
@@ -70,8 +70,8 @@ async def bamsi(ctx):
 
     await ctx.send(file = discord.File(random_image))
 bam = [
-    "Pics\Bamsi\Bamsi 1.jpg","Pics\Bamsi\Bamsi 2.jpg","Pics\Bamsi\Bamsi 3.jpg",
-    "Pics\Bamsi\Bamsi 4.jpg","Pics\Bamsi\Bamsi 5.jpg","Pics\Bamsi\Bamsi 6.jpg"
+    "Pics\\Bamsi\\Bamsi 1.jpg","Pics\\Bamsi\\Bamsi 2.jpg","Pics\\Bamsi\\Bamsi 3.jpg",
+    "Pics\\Bamsi\\Bamsi 4.jpg","Pics\\Bamsi\\Bamsi 5.jpg","Pics\\Bamsi\\Bamsi 6.jpg"
 ]
 
 @client.command(aliases = ['A']) 
