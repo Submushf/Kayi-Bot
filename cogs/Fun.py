142f553d719b9313eb2512dc76465b5e7496cd69
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2021-03-10 13:29:09 +0600

    update

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -8,15 +8,15 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['8ball'])
-    async def _8ball(self,ctx, *,member):
+    @commands.command(aliases=['8ball'], description = "just a 8ball game")
+    async def _8ball(self,ctx):
 
         responses = ["As I see it, yes", "It is certain", "Most likely", "Outlook good",
                      "Signs point to yes", "Without a doubt", "Yes", "Yes – definitely", "You may rely on it", "Reply hazy, try again",
                      "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
                      "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
     
-        await ctx.send(f'{member.mention} {random.choice(responses)}') 
+        await ctx.send(f'> 🎱 {random.choice(responses)}') 
 
 def setup(bot):
     bot.add_cog(Fun(bot))
