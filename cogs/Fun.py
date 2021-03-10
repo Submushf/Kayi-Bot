3e34342756412967f9b01f04a7ce230642c22eef
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2021-03-10 13:39:13 +0600

    update

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -16,7 +16,10 @@ class Fun(commands.Cog):
                      "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
                      "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
     
-        await ctx.send(f'> 🎱 {random.choice(responses)}') 
+        #await ctx.send(f'> 🎱 {random.choice(responses)}') 
+        embed = discord.Embed(color= 0x0F6BE2)
+        embed.add_field(name="🎱 8ball", value=f"🎱 {random.choice(responses)}")
+        await ctx.send(embed=embed) 
 
 def setup(bot):
     bot.add_cog(Fun(bot))
