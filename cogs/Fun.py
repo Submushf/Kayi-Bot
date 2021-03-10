0273a91bceaafa40ebdb3dd315c9643b93fbfb23
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2021-03-10 13:03:03 +0600

    update

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -2,14 +2,11 @@ import discord
 from discord.ext import commands
 from random import randint
 from random import choice as randchoice
-import datetime
-import time
 import asyncio
 
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.stopwatches = {}
         self.ball = ["As I see it, yes", "It is certain", "It is decidedly so", "Most likely", "Outlook good",
                      "Signs point to yes", "Without a doubt", "Yes", "Yes – definitely", "You may rely on it", "Reply hazy, try again",
                      "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
@@ -26,19 +23,6 @@ class Fun(commands.Cog):
             return await self.bot.say("`" + randchoice(self.ball) + "`")
         else:
             return await self.bot.say("That doesn't look like a question.")
-
-    @commands.command(aliases=["sw"], pass_context=True, description="just a stop watch")
-    async def stopwatch(self, ctx):
-        """Starts/stops stopwatch"""
-        author = ctx.message.author
-        if not author.id in self.stopwatches:
-            self.stopwatches[author.id] = int(time.perf_counter())
-            await self.bot.say(author.mention + " Stopwatch started!")
-        else:
-            tmp = abs(self.stopwatches[author.id] - int(time.perf_counter()))
-            tmp = str(datetime.timedelta(seconds=tmp))
-            await self.bot.say(author.mention + " Stopwatch stopped! Time: **" + str(tmp) + "**")
-            self.stopwatches.pop(author.id, None)
 
 def setup(bot):
     bot.add_cog(Fun(bot))
