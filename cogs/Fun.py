387dad8ed88fc77d747623ce5cd12a81fcb6e3b5
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2021-03-10 12:28:15 +0600

    Update Fun.py

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -1,1 +1,24 @@
-Fun.py
+import discord
+from discord.ext import commands
+import asyncio
+import random
+
+class Fun(commands.Cog):
+
+    def __init__(self, client):
+        self.client=client 
+
+	@commands.command()
+	async def 8ball(context):
+	    possible_responses = [
+	        'no way',
+	        'Prolly not',
+	        'Idk',
+	        'Prolly',
+	        'Hell yeah my',
+	    ]
+	    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
+
+
+def setup(client):
+    client.add_cog(Moderation(client))
