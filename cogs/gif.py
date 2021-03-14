a58e4f8d4351b09dc503d13e19b9b2072f011d44
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2021-03-14 11:29:03 +0600

    update

--- cogs/gif.py
+++ cogs/gif.py
@@ -9,11 +9,11 @@ class gif(commands.Cog):
         self.client = client 
 
     
-    @commands.command(description = "gives a GIf of ertugrul.") 
-    async def ertugrulg(self,ctx):
+    @commands.command(description = "gives a random GIf from the series") 
+    async def gif(self,ctx):
         ertug =  random.choice(ertugrul)
 
-        embed = discord.Embed(title=f"<:kayi:819796457942155285> Ertugrul Gif",color= 0x0F6BE2)
+        embed = discord.Embed(title=f"<:kayi:819796457942155285> Gif",color= 0x0F6BE2)
         embed.set_image(url=ertug)
 
         await ctx.send(embed=embed)
@@ -31,7 +31,26 @@ ertugrul = [
     "https://media.giphy.com/media/YPQLlzdqx3R7LnvflM/giphy.gif",
     "https://media.giphy.com/media/UrUkgydpeX8wyIMPkC/giphy.gif",
     "https://media.giphy.com/media/2yrprYMD0Tao0KCn69/giphy.gif",
-    "https://media.giphy.com/media/Me7TLPqe7zNu7srsBo/giphy.gif"
+    "https://media.giphy.com/media/Me7TLPqe7zNu7srsBo/giphy.gif",
+    "https://cdn.discordapp.com/attachments/820496784781606932/820527270434570300/gif_temp.gif",
+    "https://cdn.discordapp.com/attachments/820496784781606932/820527351178854420/gif_temp.gif",
+    "https://giphy.com/gifs/trt-network-ertugrul-dirilis-turkishdrama-MDsmaNZL19ws9HHxa3",
+    "https://giphy.com/gifs/trt-network-trt-ertugrul-video-call-UqRevvabMWsXIbQf6h",
+    "https://cdn.discordapp.com/attachments/820521610250944542/820523068949659678/QTCFfgT2dww9oOtxW3.gif",
+    "https://giphy.com/gifs/trt-network-trt-ertugrul-resurrection-StdfZEtECDCI38Ipc6",
+    "https://cdn.discordapp.com/attachments/820521610250944542/820523960990040114/lPjEfc21fGy2eQoJOv.gif",
+    "https://cdn.discordapp.com/attachments/820521610250944542/820524429196001300/Id1NYlbgCPtwRUe0tL.gif",
+    "https://giphy.com/gifs/trt-network-fight-scene-ready-to-N7ZvtDF3e94OHyHxqj",
+    "https://giphy.com/gifs/trt-network-turkish-dirilis-resurrection-ertugrul-VgrKQZ3sQl8v0izEnC",
+    "https://giphy.com/gifs/trt-network-ertugrul-dirilis-dirilisertugrul-kz0GUVisyRBp7KzW9B",
+    "https://giphy.com/gifs/trt-network-fight-dead-acting-geEVZZ0UZOwxzS1nLP",
+    "https://giphy.com/gifs/trt-network-cry-pain-weep-VDA5Xvkg1ZWPTCoWld",
+    "https://giphy.com/gifs/trt-network-slap-revenge-old-man-jVBHUatovBURllqnJp",
+    "https://giphy.com/gifs/trt-network-dirilis-eyvallah-suleymanshah-SXUXHb7oKi8tyxghxs",
+    "https://giphy.com/gifs/trt-network-9P1E8mbjhcOH3nCxdW",
+    "https://giphy.com/gifs/trt-network-fight-battle-sword-XbJN1Pupcii3c2LCUT",
+    "https://giphy.com/gifs/trt-network-fire-flame-bonfire-l28qr0dkMxmaIrmvr4",
+    "https://giphy.com/gifs/trt-network-hunger-dirilis-familydinner-ftGsJG0fXuJM9Xtq3A" 
 ]
 
 
