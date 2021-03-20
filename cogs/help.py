75d1b79adadd909cc313a62f22c224ae46408848
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2021-03-20 14:30:24 +0600

    update

--- cogs/help.py
+++ cogs/help.py
@@ -13,13 +13,13 @@ class Help(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         embed1 = discord.Embed(title= "Help" , description="Wondering where you can watch all episode [click here](https://ardirilisertugrul.net/EnErtugrul/allepisode.php)" , color = 0x0F6BE2)
-        embed1.add_field(name=f"``help :``", value=f"The help command", inline=False)
-        embed1.add_field(name=f"``mod``", value=f"The moderation commands", inline= False)
-        embed1.add_field(name=f"``Other``", value=f"Shows all the other commands", inline= False)
-        embed1.add_field(name=f"``pic``", value=f"Shows the list of character's.", inline= False) 
-        embed1.add_field(name=f"``gif``", value=f"gives a random GIf from the series.", inline= False) 
+        embed1.add_field(name=f"``help :``", value=f"-``The help command``", inline=True)
+        embed1.add_field(name=f"``mod :``", value=f"-``The moderation commands``", inline= True)
+        embed1.add_field(name=f"``Other :``", value=f"-``Shows all the other commands``", inline= True)
+        embed1.add_field(name=f"``pic :``", value=f"-``Shows the list of character's.``", inline= True) 
+        embed1.add_field(name=f"``gif :``", value=f"-``gives a random GIf from the series.``", inline= True) 
         embed1.set_thumbnail(url= "https://cdn.discordapp.com/attachments/818374423685627907/818378561189969930/kayi_bot.png")
-        embed1.set_footer(text="Page-1")
+        embed1.set_footer(text="Page-1/4")
        
         embed2 = discord.Embed(title= "Moderation",color=0x0F6BE2)
         embed2.add_field(name="clear", value=f"`clear's messages`", inline= False)
@@ -30,7 +30,7 @@ class Help(commands.Cog):
         embed2.add_field(name="avatar", value=f"`gives user avatar`", inline= False)
         embed2.add_field(name="server", value=f"`shows the amount of servers the bot is in`", inline= False)        
         embed2.set_thumbnail(url= "https://cdn.discordapp.com/attachments/818374423685627907/818378561189969930/kayi_bot.png") 
-        embed2.set_footer(text="Page-2")
+        embed2.set_footer(text="Page-2/4")
         
         embed3 = discord.Embed(title= "others" ,color=0x0F6BE2)
         embed3.add_field(name="8ball", value=f"`just a 8ball game`", inline= False)
@@ -41,7 +41,7 @@ class Help(commands.Cog):
         embed3.add_field(name="dead", value=f"`just gives a dead gif`", inline= False)
         embed3.add_field(name="random", value=f"`gives a random image`", inline= False)        
         embed3.set_thumbnail(url= "https://cdn.discordapp.com/attachments/818374423685627907/818378561189969930/kayi_bot.png") 
-        embed3.set_footer(text="Page-3")
+        embed3.set_footer(text="Page-3/4")
 
         embed4= discord.Embed(title= "Picture" , color = 0x0F6BE2)
         embed4.add_field(name="ertugrul", value= "`gives a pic of ertugrul.`", inline= False)
@@ -63,7 +63,7 @@ class Help(commands.Cog):
         embed4.add_field(name="dogan", value= "`gives a pic of dogan.`", inline= False)
         embed4.add_field(name="noyan", value= "`gives a pic of noyan.`", inline= False)
         embed4.set_thumbnail(url= "https://cdn.discordapp.com/attachments/818374423685627907/818378561189969930/kayi_bot.png") 
-        embed4.set_footer(text="Page-4")
+        embed4.set_footer(text="Page-4/4")
 
         paginator = DiscordUtils.Pagination.CustomEmbedPaginator(ctx, remove_reactions=True)
         paginator.add_reaction('◀', "back")
