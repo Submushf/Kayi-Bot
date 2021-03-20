8bbe057bdc8d67f0b31531bb7dc27fe08d19db1b
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2021-03-20 21:40:44 +0600

    update

--- cogs/help.py
+++ cogs/help.py
@@ -20,7 +20,7 @@ class Help(commands.Cog):
         embed1.add_field(name=f"Page-4", value=f"`Shows the list of character's.`", inline= False)  
         embed1.add_field(name=f"Gif", value=f"`gives a random GIf from the series.`", inline= False)
         embed1.set_thumbnail(url= "https://cdn.discordapp.com/attachments/818374423685627907/818378561189969930/kayi_bot.png")
-        embed1.set_footer(text="Page-1/4")
+        embed1.set_footer(text="Page-1/4 | react to change page")
 
         embed2 = discord.Embed(title= "Moderation",color=0x0F6BE2)
         embed2.add_field(name="clear", value=f"`clear's messages`", inline= False)
