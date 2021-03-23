a1842bdc8967b9f230080f82cc93202d0b5a44f3
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2021-03-23 22:48:52 +0600

    update

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -9,28 +9,6 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['8ball'], description = "just a 8ball game")
-    async def _8ball(self,ctx):
-
-        responses = ["As I see it, yes", "It is certain", "Most likely", "Outlook good",
-                     "Signs point to yes", "Without a doubt", "Yes", "Yes – definitely", "You may rely on it", "Reply hazy, try again",
-                     "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
-                     "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
-    
-        #await ctx.send(f'> 🎱 {random.choice(responses)}') 
-        embed = discord.Embed(title= f"🎱 {random.choice(responses)}", color= 0x0F6BE2)
-        await ctx.send(embed=embed) 
-
-    @commands.command(description= "youtube channel")
-    async def youtube(self, ctx):
-
-        embed = discord.Embed(
-            title = "Youtube", 
-            description= f"<:youtube:819620176181854238> **Subscribe to [Sana Edits](https://www.youtube.com/channel/UCMxWaxacyOX_3IgD6tHRazw)**",
-            color= 0xE21E0F
-            )
-        await ctx.send(embed=embed)
-        
     @commands.command(aliases=['s'],description="says what you say")
     async def say(self,ctx,*,message):
         await ctx.send(f'**{message}** \n\n-**{ctx.message.author}**')
